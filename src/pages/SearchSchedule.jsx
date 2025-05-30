ea713869f0ff2a96793d05a8d62b5dd043c8e5f4
Lepocka <mykola.barannyk@gmail.com>
2025-05-30 21:23:55 +0300

    2 pagestest

---
+++ src/pages/SearchSchedule.jsx
@@ -0,0 +1,234 @@
+import React from "react";
+import "../styles/SearchSchedule.css";
+
+export const SearchSchedule = () => {
+  return (
+    <div className="search-schedule">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="find-schedule">
+            <div className="overlap-group">
+              <div className="text-wrapper">Пошук розкладу</div>
+
+              <div className="div">Оберіть викладача:</div>
+
+              <div className="overlap-2">
+                <div className="text-wrapper-2">Вівторок</div>
+
+                <div className="overlap-3">
+                  <img
+                    className="line"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-33.svg"
+                  />
+
+                  <img
+                    className="img"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-44.svg"
+                  />
+                </div>
+              </div>
+
+              <div className="overlap-4">
+                <div className="overlap-5">
+                  <img
+                    className="img"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-44.svg"
+                  />
+
+                  <img
+                    className="line"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-33.svg"
+                  />
+                </div>
+
+                <div className="text-wrapper-3">Шпак З. Я. |</div>
+              </div>
+
+              <div className="overlap-6">
+                <div className="overlap-7">
+                  <img
+                    className="line-2"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-35.svg"
+                  />
+
+                  <img
+                    className="line"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-36.svg"
+                  />
+                </div>
+
+                <div className="text-wrapper-4">Понеділок</div>
+              </div>
+
+              <div className="overlap-8">
+                <div className="text-wrapper-2">П’ятниця</div>
+
+                <div className="overlap-9">
+                  <img
+                    className="line"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-33.svg"
+                  />
+
+                  <img
+                    className="img"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-44.svg"
+                  />
+                </div>
+              </div>
+
+              <div className="overlap-10">
+                <div className="text-wrapper-2">Четвер</div>
+
+                <div className="overlap-11">
+                  <img
+                    className="line"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-33.svg"
+                  />
+
+                  <img
+                    className="img"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-44.svg"
+                  />
+                </div>
+              </div>
+
+              <div className="overlap-12">
+                <div className="text-wrapper-2">Середа</div>
+
+                <div className="overlap-3">
+                  <img
+                    className="line"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-33.svg"
+                  />
+
+                  <img
+                    className="img"
+                    alt="Line"
+                    src="https://c.animaapp.com/mbb4fsikolLrbZ/img/line-44.svg"
+                  />
+                </div>
+              </div>
+
+              <p className="element">
+                <span className="span">🕑 Час:</span>
+
+                <span className="text-wrapper-5"> 8:30 - 9:5</span>
+
+                <span className="span">0</span>
+              </p>
+
+              <p className="p">
+                <span className="text-wrapper-6">📚</span>
+
+                <span className="text-wrapper-7"> Предмет:</span>
+
+                <span className="text-wrapper-8"> Основи програмування</span>
+
+                <span className="text-wrapper-6">&nbsp;</span>
+              </p>
+
+              <p className="i">
+                <span className="text-wrapper-9">📍</span>
+
+                <span className="text-wrapper-7"> Аудиторія</span>
+
+                <span className="text-wrapper-10">:</span>
+
+                <span className="text-wrapper-8"> I н. к., 118 каб</span>
+
+                <span className="text-wrapper-9">. </span>
+              </p>
+            </div>
+          </div>
+
+          <div className="news-section">
+            <img
+              className="image"
+              alt="Image"
+              src="https://c.animaapp.com/mbb4fsikolLrbZ/img/image-20.png"
+            />
+
+            <p className="text-wrapper-11">
+              AR для освіти і культури: укладено угоду про співпрацю між
+              Політехнікою, компанією Skeiron та Домом Франка
+            </p>
+
+            <div className="div-wrapper">
+              <div className="text-wrapper-12">Новини кафедри</div>
+            </div>
+
+            <img
+              className="image-2"
+              alt="Image"
+              src="https://c.animaapp.com/mbb4fsikolLrbZ/img/image-21.png"
+            />
+
+            <p className="text-wrapper-13">
+              Робофутбол від кафедри автоматизованих систем управління
+            </p>
+          </div>
+
+          <div className="hero">
+            <div className="overlap-13">
+              <div className="text-wrapper-14">
+                КАФЕДРА АВТОМАТИЗОВАНИХ СИСТЕМ УПРАВЛІННЯ
+              </div>
+            </div>
+          </div>
+
+          <div className="navbar">
+            <div className="overlap-14">
+              <div className="rectangle" />
+
+              <div className="rectangle-2" />
+
+              <div className="rectangle-3" />
+
+              <div className="rectangle-4" />
+
+              <div className="text-wrapper-15">
+                &nbsp;&nbsp;&nbsp;&nbsp;Головна&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Пошук
+                викладача&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Пошук
+                розкладу&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Запис
+              </div>
+            </div>
+          </div>
+
+          <footer className="footer">
+            <div className="overlap-15">
+              <p className="element-2">
+                Адреса:
+                <br />
+                вул. С. Бандери 28а, 5-й н.к., кім. 801
+              </p>
+
+              <div className="element-3">
+                Телефон:
+                <br />
+                (032) 258-26-47
+              </div>
+
+              <div className="e-mail-asu-dept-lpnu">
+                E-mail:
+                <br />
+                asu.dept@lpnu.ua
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+};
